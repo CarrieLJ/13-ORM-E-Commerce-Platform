4ea378e7714ad1ea489c97fdc70a492ca466e05e
Mona Lisa <cjensen@netechnical.com>
2023-01-30 09:19:09 -0600

    removed code that was not needed within the schema file

--- db/schema.sql
+++ db/schema.sql
@@ -3,9 +3,3 @@ DROP DATABASE IF EXISTS ecommerce_db;
 
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
-
-CREATE TABLE category {
-    id INT PRIMARY KEY AUTO INCREMENT NOT NULL,
-    category_name STRING NOT NULL
-}
-
